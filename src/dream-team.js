863b744d43ee7c6fe2f37f93eefe848c426e98d8
Kateryna Nanyzniuk <katerynananyzniuk@gmail.com>
2021-09-29 11:46:07 +0300

    fix: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -16,6 +16,6 @@ import { NotImplementedError } from '../extensions/index.js';
 export default function createDreamTeam(members) {
   let dreamTeam = []
   members.forEach((item)=>{if (typeof(item)==="string"){return dreamTeam.push(item[0])}})
-  dreamTeam.sort()
+  dreamTeam = dreamTeam.sort().join("")
   return dreamTeam
 }
