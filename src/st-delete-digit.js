9cce5352ea019e61cbf5d11d688a862142cd1545
Kateryna Nanyzniuk <katerynananyzniuk@gmail.com>
2021-10-01 11:26:28 +0300

    feat: done st-delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n){
+  let arrFromN = n.toString().split('')
+  let arrCopy = n.toString().split('')
+  arrCopy = arrCopy.sort(function(a, b) { return a - b; })
+  let number = arrCopy.slice(0,1)[0]
+  arrFromN.find(function(item,index){
+    if(item===number){ arrFromN.splice(index,1)}
+  })
+  return +arrFromN.join('')
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  
+  if (!Array.isArray(arr)){ throw new Error("'arr' parameter must be an instance of the Array!")
+}
+else {
+    let next = "--discard-next"
+    let prev = "--discard-prev"
+    let dnext = "--double-next"
+    let dprev = "--double-prev"
+    let array = arr.slice()
+    array.find((item)=>{
+      let index = array.findIndex((item)=>{
+        return item===next || item===prev || item===dnext || item===dprev
+      })
+
+      if (typeof(index) !== 'number') { array.splice(item,1)}
+
+      else if (item === next){ array.splice(index,2)}
+
+      else if (item === prev){ 
+        if(index>0){ array.splice(index-1,2)} 
+        else {array.splice(index,1)}
+      }
+
+      else if (item === dnext){ 
+        if(index === array.length-1){ array.splice(index,1)} 
+        else {array.splice(index,1,array[index+1])}
+      }
+        
+      else if (item === dprev){
+         if(index>0){array.splice(index,1,array[index-1])} 
+         else {array.splice(index,1)}
+      }
+    })
+    return array
+  }
 }
